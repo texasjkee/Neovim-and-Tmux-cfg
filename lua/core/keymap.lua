b25c93b58a7f33b8bf1c7d532e6711f23afe624d
Michael <gt.gluck@gmail.com>
2023-06-12 10:17:42 +0300

    delete: old config

--- lua/core/keymap.lua
+++
@@ -1,4 +0,0 @@
-vim.keymap.set('n', '<C-v>', '"*p<CR>')
-vim.keymap.set('v', '<C-c>', '"*y<CR>')
-vim.keymap.set('n', '<C-h>', ':nohlsearch<CR>')
-
--- lua/core/plugin_config/nvim-tree.lua
+++
@@ -1,5 +0,0 @@
-vim.g.loaded_netrw = 1
-vim.g.loaded_netrwPlugin = 1
-vim.keymap.set('n', '<c-e>', ':NvimTreeToggle<CR>')
-require("nvim-tree").setup()
-
